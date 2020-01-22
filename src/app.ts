fb85f9f70f685e885120c2ab44472f0b5bfd5911
Jru Harris <jru@jruharris.com>
2020-01-21 22:03:24 -0500

    strong typing for classes. start generating html

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,29 @@
-const axios = require('axios')
+import axios from 'axios';
 
 (async () => {
-    const schedule = getSchedule()
-
-    // generate html
+    const classes = await getClasses();
+    const html = generateHtml(classes);
+    addHtmlToPage(html);
 })()
 
-async function getSchedule() {
-    const { today, tenDaysFromToday } = getDates()
-    const uri = `https://giveyoga.marianatek.com/api/class_sessions?include=in_booking_window&location=48056&min_date=${today}&max_date=${tenDaysFromToday}&ordering=start_datetime&page_size=100`
-    const schedule = (await axios.get(uri)).data
+async function getClasses() {
+    const schedule = await fetchSchedule();
+    return schedule.map((c: any) => toClass(c)) as Class[];
+}
 
-    // strongly type this schedule
-
-    return schedule
+async function fetchSchedule() {
+    const { today, tenDaysFromToday } = getDates();
+    const uri = `https://giveyoga.marianatek.com/api/class_sessions?include=in_booking_window&location=48056&min_date=${today}&max_date=${tenDaysFromToday}&ordering=start_datetime&page_size=100`;
+    const schedule = (await axios.get(uri)).data.data;
+    return schedule;
 }
 
 function getDates() {
-    let date = new Date()
-    const today = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
-    date = addDays(date, 9)
-    const tenDaysFromToday = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
-    return { today, tenDaysFromToday }
+    let date = new Date();
+    const today = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    date = addDays(date, 9);
+    const tenDaysFromToday = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    return { today, tenDaysFromToday };
 }
 
 function addDays(originalDate: Date, days: number) {
@@ -29,3 +31,29 @@ function addDays(originalDate: Date, days: number) {
     newDate.setDate(newDate.getDate() + days);
     return newDate;
 }
+
+function toClass(classEntity: any) {
+    return {
+        startDateTime: new Date(classEntity.attributes.start_datetime),
+        room: classEntity.attributes.classroom_display,
+        name: classEntity.attributes.class_type_display,
+        instructor: classEntity.attributes.instructor_names.join(' - ')
+    } as Class;
+}
+
+function generateHtml(classes: Class[]) {
+    let html = '';
+    classes.forEach((c) => html += `<li>${c.name}; ${c.startDateTime}; ${c.instructor}; ${c.room}</li>`);
+    return html;
+}
+
+function addHtmlToPage(html: string) {
+    document.getElementById('classList').innerHTML = html;
+}
+
+type Class = {
+    startDateTime: Date;
+    room: string;
+    name: string;
+    instructor: string;
+};
