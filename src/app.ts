0736effb93735ba2a0863966960a49d844be09e2
Jru Harris <jru@jruharris.com>
2020-02-22 11:20:07 -0500

    group by date

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 (async () => {
     const classes = await getClasses();
-    const html = generateHtml(classes);
+    const classesGroupedByDay = groupByDay(classes);
+    const html = generateHtml(classesGroupedByDay);
     addHtmlToPage(html);
 })();
 
@@ -16,6 +17,17 @@ async function fetchSchedule() {
     const responseData = await response.json();
     const schedule = responseData.data;
     return schedule;
+}
+
+function groupByDay(classes: Class[]) {
+    const days = classes.map((c) => c.startDateTime.getDate());
+    const uniqueDays = [...new Set(days)];
+    const groupedClasses: Class[][] = [];
+    uniqueDays.forEach(day => {
+        const classesForDay = classes.filter(c => c.startDateTime.getDate() === day);
+        groupedClasses.push(classesForDay);
+    });
+    return groupedClasses;
 }
 
 function getDates() {
@@ -41,16 +53,25 @@ function toClass(classEntity: any) {
     } as Class;
 }
 
-function generateHtml(classes: Class[]) {
+function generateHtml(classesGroupedByDay: Class[][]) {
     let html = '';
-    const options = { weekday: 'short', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-    classes.forEach((c) => html += `<li>${c.name}; ${c.startDateTime.toLocaleDateString('en-US', options)}; ${c.instructor}; ${c.room}</li>`);
+    
+    classesGroupedByDay.forEach((c) => html += generateDayHtml(c));
     return html;
+}
+
+function generateDayHtml(classes: Class[]) {
+    let html = `<div class=\'\'><h2>${classes[0].startDateTime.toLocaleDateString('en-US', options)}</h2><ul>`;
+    classes.forEach(c => html += `<li>${c.startDateTime.toLocaleTimeString('en-US', dayOptions)}; ${c.name}; ${c.instructor}; ${c.room}</li>`)
+    return html + '</ul></div>';
 }
 
 function addHtmlToPage(html: string) {
     document.getElementById('schedule')!.innerHTML = html;
 }
+
+const options = { weekday: 'short', month: 'short', day: 'numeric'};
+const dayOptions = { hour: 'numeric', minute: 'numeric' };
 
 type Class = {
     startDateTime: Date;
